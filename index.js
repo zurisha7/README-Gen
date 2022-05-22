79cf37bf98a388aed7573b71e8df02678bbcab5d
Zurisha <andreamwest7@gmail.com>
2022-05-21 21:19:44 -0500

    questions added

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-const inquirer = require('inquirer')
+const fs = require('fs');
+ const inquirer = require('inquirer')
+
 
 //TODO: Create an array of questions for user input
-const questions = [
-    inquirer
+const promptQuestions = () => {
+     return inquirer
     .prompt ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project.'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a detailed description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the steps to install the project.'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: "Enter details of the project's features, interface, and tips you would like to include."
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github link.'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Enter the Contributors of your project.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license for your project. Please only choose one',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU General Public License v2.0', 'Mozilla Public License 2.0', 'The Unlicense', 'NO LICENSE']
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'Please enter your name.'
+    }
             ])
-   
-       
-];
-
-
+        
+        };
 // TODO: Create a function to write README file
 function writeToFile(README, data) {
     return `
@@ -45,11 +96,11 @@ function writeToFile(README, data) {
 </div>
 <div>
       <h2>Installation<h2>
-      <h3>${}</h3>
+      <h3>${installation}</h3>
 </div>
 <div>
     <h2>Usage</h2>
-    <h3>${}</h3>
+    <h3>${usage}</h3>
 </div>
 <div>
     <h2>License<h2>
@@ -65,7 +116,7 @@ function writeToFile(README, data) {
     <h3>${Test}</h3>
 <div>
     <h2>Questions</h2>
-    <h3>If you have any questions please contact ${name} at ${contact} or on GitHub at ${github}.<h3>
+    <h3>If you have any questions please contact ${author} at ${contact} or on GitHub at ${github}.<h3>
 
 
     </body>
@@ -76,7 +127,9 @@ function writeToFile(README, data) {
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptQuestions();
+}
 
 // Function call to initialize app
 init();
