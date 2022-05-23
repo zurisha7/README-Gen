cae145207d4b4971f78b92c243e4dd0a0113d702
Zurisha <andreamwest7@gmail.com>
2022-05-23 16:08:17 -0500

    updated license

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
  const inquirer = require('inquirer')
-
+ const writeToFile = require('./src/page-template')
+const renderLicenseBadge = require('./utilities/generateReadMe')
 
 //TODO: Create an array of questions for user input
 const promptQuestions = () => {
@@ -9,7 +10,15 @@ const promptQuestions = () => {
     {
         type: 'input',
         name: 'title',
-        message: 'Enter the title of your project.'
+        message: 'Enter the title of your project.',
+        validate: titleInput => {
+            if(titleInput) {
+                return true;
+            }else{
+                console.log('Please enter your project title!');
+                return false;
+            }
+        }        
     },
     {
         type: 'input',
@@ -23,7 +32,7 @@ const promptQuestions = () => {
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: "Enter details of the project's features, interface, and tips you would like to include."
     },
     {
@@ -35,7 +44,16 @@ const promptQuestions = () => {
     {
         type: 'input',
         name: 'github',
-        message: 'Enter your github link.'
+        message: 'Enter your github username.',
+        validate: githubInput => {
+            if(githubInput) {
+                return true;
+            }else{
+                console.log('Please enter your github link!');
+                return false;
+            }
+        }        
+
     },
     {
         type: 'input',
@@ -51,84 +69,52 @@ const promptQuestions = () => {
         type: 'checkbox',
         name: 'license',
         message: 'Choose a license for your project. Please only choose one',
-        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU General Public License v2.0', 'Mozilla Public License 2.0', 'The Unlicense', 'NO LICENSE']
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU General Public License v2.0', 'Mozilla Public License 2.0', 'The Unlicense'],
     },
     {
         type: 'input',
         name: 'author',
-        message: 'Please enter your name.'
+        message: 'Please enter your name.',
+        validate: authorInput => {
+            if(authorInput) {
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
             ])
+        .then(data=> {
+            console.log(data)
         
-        };
+        })
+            // .then(projectData => {
+            //     portfolioData.projects.push(projectData);
+            //     if(projectData.confirmAddProject) {
+            //       return promptProject(porfolioData);
+            //     } else {
+            //       return portfolioData;
+            //     }
+            //     });
+             };
+             writeToFile();
+       fs.writeFile('./index.html', writeToFile()), err =>{
+       if(err)throw new Error(err);
+
+       console.log('README complete! Look at index.html to see the finished product!')
+     
+       
+
+       }
+
+
 // TODO: Create a function to write README file
-function writeToFile(README, data) {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>${title}</title>
-    </head>
-  
-    <body>
-      <h1>${title}</h1>
 
-    <div>    
-      <h2>Table of Contents</h2>
-      <ul>
-        <li>Description</li>
-        <li>Installation</li>
-        <li>Usage</li>
-        <li>License</li>
-        <li>Contributors</li>
-        <li>Tests</li>
-        <li>Questions</li>
- </div> 
-<div>
-      <h2>Description</h2>
-      <h4>${description}</h4>
-      <h3>${id}</h3>
-      <h3><a href="${email}"
-      <h3><a href="https://github.com/${github}">Github</a></h2>
-</div>
-<div>
-      <h2>Installation<h2>
-      <h3>${installation}</h3>
-</div>
-<div>
-    <h2>Usage</h2>
-    <h3>${usage}</h3>
-</div>
-<div>
-    <h2>License<h2>
-    <h3>${license}</h3>
-    <h4>${licensedescription}</h4>
-</div>
-<div>
-    <h2>Contributors<h2>
-    <h3>${contributors}</h3>
-</div>
-<div>
-    <h2>Tests<h2>
-    <h3>${Test}</h3>
-<div>
-    <h2>Questions</h2>
-    <h3>If you have any questions please contact ${author} at ${contact} or on GitHub at ${github}.<h3>
-
-
-    </body>
-    </html>
-    `;
-  };
-    
 
 
 // TODO: Create a function to initialize app
 function init() {
-    promptQuestions();
+   promptQuestions();
 }
 
 // Function call to initialize app
--- utilities/generateReadMe.js
+++ utilities/generateReadMe.js
@@ -1,20 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//TODO: Create a function that returns a license badge based on which license is passed in
+//If there is no license, return an empty string
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license.license; // i think this is problem #1
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+ }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+  return `# ${data.title} `;
+ }
 
 module.exports = generateMarkdown;
